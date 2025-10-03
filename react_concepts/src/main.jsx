97b052210ff2c8320609692436d8994eaaf8d5f4
Manish10022001 <manishshirsat2850@gmail.com>
2025-10-03 11:27:37 +0530

    data binding with undefined type

--- react_concepts/src/main.jsx
+++ react_concepts/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import LoginComponent from "./components/login/login.component.jsx";
-import DataBindingWithString from "./components/data-binding/data-binding-3.jsx";
+import DataBindingWithUndefinedType from "./components/data-binding/data-binding-4.jsx";
 
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
@@ -13,6 +13,6 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     {/* <LoginComponent /> */}
-    <DataBindingWithString />
+    <DataBindingWithUndefinedType />
   </StrictMode>
 );
