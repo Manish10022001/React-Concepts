d537867fec59fd8397b2576abf88115326e659a9
Manish10022001 <manishshirsat2850@gmail.com>
2025-10-05 17:48:52 +0530

    axios to get data

--- react_concepts/src/main.jsx
+++ react_concepts/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import LoginComponent from "./components/login/login.component.jsx";
 import DataBindingWithArrays from "./components/data-binding/data-binding-5.jsx";
 import AjaxWithjQuery from "./components/ajax/ajax-3.jsx";
+import AxiosOne from "./components/axios/axios-1.jsx";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap";
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")).render(
     {/* <App /> */}
     {/* <LoginComponent /> */}
     {/* <DataBindingWithArrays /> */}
-    <AjaxWithjQuery />
+    {/* <AjaxWithjQuery /> */}
+    <AxiosOne />
   </StrictMode>
 );
