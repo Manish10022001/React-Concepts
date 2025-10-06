a5ad8303e085564567ddcbb6e86aeb65891e8566
Manish10022001 <manishshirsat2850@gmail.com>
2025-10-06 11:37:02 +0530

    axios example

--- react_concepts/src/main.jsx
+++ react_concepts/src/main.jsx
@@ -6,6 +6,7 @@ import LoginComponent from "./components/login/login.component.jsx";
 import DataBindingWithArrays from "./components/data-binding/data-binding-5.jsx";
 import AjaxWithjQuery from "./components/ajax/ajax-3.jsx";
 import AxiosOne from "./components/axios/axios-1.jsx";
+import Courses from "./components/axios/courses.jsx"
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap";
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")).render(
     {/* <LoginComponent /> */}
     {/* <DataBindingWithArrays /> */}
     {/* <AjaxWithjQuery /> */}
-    <AxiosOne />
+    <Courses />
+    {/* <AxiosOne /> */}
   </StrictMode>
 );
