3b3e9cebeab326f6e6085bb7682ff0930662f817
Manish10022001 <manishshirsat2850@gmail.com>
2025-11-20 15:26:27 +0530

    controlled and uncontrolled components

--- react_concepts/src/main.jsx
+++ react_concepts/src/main.jsx
@@ -17,7 +17,7 @@ import "bootstrap";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <LoginComponent /> */}
     {/* <DataBindingWithArrays /> */}
     {/* <AjaxWithjQuery /> */}
@@ -26,6 +26,6 @@ createRoot(document.getElementById("root")).render(
     {/* <NestedDemo /> */}
     {/* <MouseEventTwo /> */}
     {/* <FormikValidationState /> */}
-    <ClipboardEvents />
+    {/* <ClipboardEvents /> */}
   </StrictMode>
 );
