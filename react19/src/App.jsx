1da2329f9d18cb10723fef1e8b7998a3dc2cee9b
Manish10022001 <manishshirsat2850@gmail.com>
2025-12-12 00:48:58 +0530

    Props: Default Props

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Button from './component/Button'
+import { Greeting } from './component/Greeting'
 import { Hello, HelloWithoutJsx } from './component/Hello'
 import { Product } from './component/Product'
 import { Welcome } from './component/Welcome'
@@ -7,6 +8,11 @@ import { Welcome } from './component/Welcome'
 function App() {
   return (
     <>
+      <Greeting name="Bruce" message="Good Morning"/>
+      <Greeting name="Clark"/>
+      <Greeting message="Welcome"/>
+      <Greeting />
+      
       <Product title="Gaming Laptop" price={3343} inStock={true} category={["electronics", "Computes", "Gaming"]}/>
       <Welcome name="Bruce" alias="Batman"/>
       <Welcome name="Clark" alias="Superman"/>
---
+++ react19/src/component/Greeting.jsx
@@ -0,0 +1,4 @@
+//Default Props
+export function Greeting({name="Guest", message="Hello"}){
+    return <h1>{name}, {message}</h1>
+}
