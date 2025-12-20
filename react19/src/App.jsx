6e8e62a275089e55f0a30597b8a42362bb06e9a7
Manish10022001 <manishshirsat2850@gmail.com>
2025-12-20 12:59:37 +0530

    resolved conflicts

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -14,6 +14,7 @@ import { ProductList } from './renderingList/ProductList'
 import { Alert } from './stylingReactComponent/Alert'
 import { Counter } from './Hooks/useState/Counter'
 import { LoginCard } from './Hooks/useState/LoginCard'
+import { PrevStateCounter } from './Hooks/useState/PrevStateCounter'
 import { SimpleCounter } from './Hooks/useState/SimpleCounter'
 
 function App() {
